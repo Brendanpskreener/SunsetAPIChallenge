c572bb8703ef2198ee8ebee4f9d192907e997107
Brendan <brendanpschreiner@gmail.com>
2023-02-20 20:02:45 -0800

    created sunrise and sunset function

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 import got from 'got';
 
-function getLatLong (city, state, country = 'US') {
+//Add feature to pass in city/state from the terminal
+
+async function getLatLong (city, state, country = 'US') {
     const { API_NINJA_URL, API_NINJA_API_KEY} = process.env
     const options = {
         headers: {
@@ -14,11 +16,34 @@ function getLatLong (city, state, country = 'US') {
             country
         }
     }
-    return got(API_NINJA_URL, options).json();
+    const [result] = await got(API_NINJA_URL, options).json()
+    const { latitude, longitude } = result
+    return {
+        latitude,
+        longitude
+    }
+}
+
+async function getSunriseSunset (latitude, longitude) {
+    const { API_SUNRISE_SUNSET_URL } = process.env
+    const options = {
+        searchParams: {
+            lat: latitude,
+            lng: longitude,
+            formatted: 0
+        }
+    }
+    const { results: { sunrise, sunset } } = await got(API_SUNRISE_SUNSET_URL, options).json()
+    return {
+        sunrise: new Date(sunrise).toLocaleString(),
+        sunset: new Date(sunset).toLocaleString()
+    }
 }
 
 try {
-    console.log(await getLatLong('Portland', 'Oregon'))
+    const latLong = await getLatLong('Portland', 'Oregon')
+    const sunTime = await getSunriseSunset(latLong.latitude, latLong.longitude)
+    console.log(sunTime)
 } catch (error) {
     console.error(error)
 }
